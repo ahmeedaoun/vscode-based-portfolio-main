6b3239a6696b37e5d1be167dad9def3797175255
AhmedAoun147 <109686263+AhmedAoun147@users.noreply.github.com>
2025-08-16 15:04:27 +0100

    added project

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -1,3 +1,11 @@
 export const projects = [
   
+    {
+    ImageURL: "/assets/project-vscode.jpg",
+    Title: "VSCode Themed Portfolio",
+    Description: "My personal portfolio with the Visual Studio Code IDE theme.",
+    Technologies: ["react", "typescript", "chakra-ui", "responsive"],
+    Source: "https://github.com/ahmeedaoun/vscode-based-portfolio-main",
+    Demo: "",
+  },
     ];
